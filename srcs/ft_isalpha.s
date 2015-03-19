c0cc1e6db9e060660b71697acf73de7afe875154
etermeau <etermeau@student.42.fr>
2015-03-19 19:29:18 +0100

    ft_isalpha.s fonctionne

--- srcs/ft_isalpha.s
+++ srcs/ft_isalpha.s
@@ -2,33 +2,33 @@ section .text
 	global _ft_isalpha
 
 _ft_isalpha:
-	push rsp
-	mov rsp, rdi
-	cmp rsp, 65 ; = 'A'
+	push rbx
+	mov ebx, edi
+	cmp ebx, 65 ; = 'A'
 	jge is_maj ; >=
 	jl is_false ; <
 
 is_maj:
-	cmp rsp, 90 ; = 'Z'
+	cmp ebx, 90 ; = 'Z'
 	jle is_true ; <=
 	jg is_not_maj ; >
 
 is_not_maj:
-	cmp rsp, 97 ; = 'a'
+	cmp ebx, 97 ; = 'a'
 	jge is_min ; >=
 	jl is_false ; <
 
 is_min:
-	cmp rsp, 122 ; = 'z'
+	cmp ebx, 122 ; = 'z'
 	jle is_true ; <=
 	jg is_false ; >
 
 is_false:
-	mov rax, 0
-	pop rsp
+	mov eax, 0
+	pop rbx
 	ret
 
 is_true:
-	mov rax, 1
-	pop rsp
+	mov eax, 1
+	pop rbx
 	ret
